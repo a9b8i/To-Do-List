e24d90d550ebfe97d1ca888683371a32f28d0eb6
Abdul B. Issa <abdulbadi.issa@gmail.com>
2022-05-22 22:12:08 -0500

    ‘update’

--- db/models/task.js
+++ db/models/task.js
@@ -35,23 +35,71 @@ module.exports = (sequelize, DataTypes) => {
   const Task = sequelize.define('Task', {
     taskName: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please provide a value for Task',
+        },
+        notEmpty: {
+          msg: 'Please provide a value for Task',
+        },
+        len: {
+          args: [0, 255],
+          msg: 'Title must not be more than 255 characters long',
+        }
+      }
     },
     personName:{
       type: DataTypes.STRING(100),
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please provide a value for Author',
+        },
+        notEmpty: {
+          msg: 'Please provide a value for Author',
+        },
+        len: {
+          args: [0, 100],
+          msg: 'Author must not be more than 100 characters long',
+        }
+      }
     },
     startDate: {
       type: DataTypes.DATEONLY,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please provide a value for Release Date',
+        },
+        isDate: {
+          msg: 'Please provide a valid date for Release Date',
+        }
+      }
     },
     deadline:  {
       type: DataTypes.DATEONLY,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please provide a value for Release Date',
+        },
+        isDate: {
+          msg: 'Please provide a valid date for Release Date',
+        }
+      }
     },
     hours: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please provide a value for Page Count',
+        },
+        isInt: {
+          msg: 'Please provide a valid integer for Page Count',
+        }
+      }
     }
   }, {});
   Task.associate = function(models) {
