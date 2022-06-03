330b221b8792207942dddda8dac004fa9be11b59
Abdul B. Issa <abdulbadi.issa@gmail.com>
2022-06-02 22:33:32 -0500

    ‘update’

--- db/models/task.js
+++ db/models/task.js
@@ -1,105 +1,25 @@
-// 'use strict';
-// module.exports = (sequelize, DataTypes) => {
-//   const Task = sequelize.define('Task', {
-//     taskName: {
-//       type: DataTypes.STRING,
-//       allowNull: false
-//     },
-//     personName:{
-//       type: DataTypes.STRING(100),
-//       allowNull: false
-//     },
-//     startDate: {
-//       type: DataTypes.DATEONLY,
-//       allowNull: false
-//     },
-//     deadline:  {
-//       type: DataTypes.DATEONLY,
-//       allowNull: false
-//     },
-//     hours: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false
-//     }
-//   }, {});
-//   Task.associate = function(models) {
-//     // associations can be defined here
-//   };
-//   return Task;
-// };
-
-
-
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Task = sequelize.define('Task', {
     taskName: {
       type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notNull: {
-          msg: 'Please provide a value for Task',
-        },
-        notEmpty: {
-          msg: 'Please provide a value for Task',
-        },
-        len: {
-          args: [0, 255],
-          msg: 'Title must not be more than 255 characters long',
-        }
-      }
+      allowNull: false
     },
     personName:{
       type: DataTypes.STRING(100),
-      allowNull: false,
-      validate: {
-        notNull: {
-          msg: 'Please provide a value for Author',
-        },
-        notEmpty: {
-          msg: 'Please provide a value for Author',
-        },
-        len: {
-          args: [0, 100],
-          msg: 'Author must not be more than 100 characters long',
-        }
-      }
+      allowNull: false
     },
     startDate: {
       type: DataTypes.DATEONLY,
-      allowNull: false,
-      validate: {
-        notNull: {
-          msg: 'Please provide a value for Release Date',
-        },
-        isDate: {
-          msg: 'Please provide a valid date for Release Date',
-        }
-      }
+      allowNull: false
     },
     deadline:  {
       type: DataTypes.DATEONLY,
-      allowNull: false,
-      validate: {
-        notNull: {
-          msg: 'Please provide a value for Release Date',
-        },
-        isDate: {
-          msg: 'Please provide a valid date for Release Date',
-        }
-      }
+      allowNull: false
     },
     hours: {
       type: DataTypes.INTEGER,
-      allowNull: false,
-      validate: {
-        notNull: {
-          msg: 'Please provide a value for Page Count',
-        },
-        isInt: {
-          msg: 'Please provide a valid integer for Page Count',
-        }
-      }
+      allowNull: false
     }
   }, {});
   Task.associate = function(models) {
@@ -107,4 +27,3 @@ module.exports = (sequelize, DataTypes) => {
   };
   return Task;
 };
-
