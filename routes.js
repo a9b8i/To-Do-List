cc891a82e286342de4b612ea5fa3a49206fe650d
Abdul B. Issa <abdulbadi.issa@gmail.com>
2022-09-08 10:26:19 -0500

    ‘update’

--- routes.js
+++ routes.js
@@ -3,8 +3,14 @@ const express = require('express');
 const router = express.Router();
 const db = require('./db/models');
 
-router.get('/', (req, res) => {
-  res.render('index', { title: 'Home' });
+
+router.get('/', async (req, res, next) => {
+  try {
+    const tasks = await db.Task.findAll({ order: [['taskName', 'ASC']] });
+    res.render('index', { title: 'Home', tasks });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
