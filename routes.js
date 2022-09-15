8f0f030baa5e5644111733646565dcac1e49ac20
Abdul B. Issa <abdulbadi.issa@gmail.com>
2022-09-14 21:23:24 -0500

    ‘update’

--- routes.js
+++ routes.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  throw new Error('This is a test error!');
   res.render('index', { title: 'Home' });
 });
 
