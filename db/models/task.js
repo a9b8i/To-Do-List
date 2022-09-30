73d6c29217de26b122797deeca44b603875002d8
Abdul B. Issa <abdulbadi.issa@gmail.com>
2022-09-30 10:12:01 -0500

    update

--- db/models/task.js
+++ db/models/task.js
@@ -1,15 +1,29 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Task = sequelize.define('Task', {
-    taskName: DataTypes.STRING,
-    personName: DataTypes.STRING,
-    startDate: DataTypes.DATEONLY,
-    deadline: DataTypes.DATEONLY,
-    hours: DataTypes.INTEGER
+    taskName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    personName:{
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
+    startDate: {
+      type: DataTypes.DATEONLY,
+      allowNull: false
+    },
+    deadline:  {
+      type: DataTypes.DATEONLY,
+      allowNull: false
+    },
+    hours: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
   }, {});
   Task.associate = function(models) {
     // associations can be defined here
   };
   return Task;
 };
-
